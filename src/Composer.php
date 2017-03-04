4d27789ce7efbfa182bae0e7c3a85008fcdeab15
Geoffrey Laurent <geoffrey.laurent@rueducommerce.com>
2017-03-04 12:56:37 +0100

    modif

--- src/Composer.php
+++ src/Composer.php
@@ -5,7 +5,7 @@ use Composer\Script\Event;
 class Composer {
 	public static function integrate( Event $event ) {
 		$event->getIO()->write("Show me after INSTALL/UPDATE command");
-		if ( copy ( 'grumphp.yml' , $event->getComposer()->getConfig()->get('vendor-dir') . '/../grumphp.yml' ) ) {
+		if ( copy ( 'config/grumphp.yml' , $event->getComposer()->getConfig()->get('vendor-dir') . '/../grumphp.yml' ) ) {
 			$event->getIO()->write('<fg=green>Copie du fichier grumphp.yml effectuée</fg=green>');
 		} else {
 			$event->getIO()->write('<fg=red>Erreur lors de la copie du fichier grumphp.yml</fg=red>');
