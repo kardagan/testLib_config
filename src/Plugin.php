8e956abd9c3ecf3746e033d1519d40e112f63b5f
Geoffrey Laurent <geoffrey.laurent@rueducommerce.com>
2017-03-05 16:54:05 +0100

    modif

--- src/Plugin.php
+++ src/Plugin.php
@@ -15,6 +15,9 @@ use Composer\Script\ScriptEvents;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
+    const PACKAGE_NAME = 'kardagan/testlibconf';
+    const FILE_NAME = 'grumphp.yml';
+
     /**
      * @var Composer
      */
@@ -98,23 +101,33 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         $this->copyConfigFile($event);
     }
 
-    public static function copyConfigFile(PackageEvent $event)
+    private function copyConfigFile(PackageEvent $event)
     {
-        $event->getIO()->write("<fg=white>Copie du fichier grumphp.yml à la racine.</fg=white>");
-        if (copy('config/grumphp.yml', $event->getComposer()->getConfig()->get('vendor-dir') . '/../grumphp.yml')) {
-            $event->getIO()->write('<fg=green>Copie du fichier grumphp.yml effectuée</fg=green>');
+        $event->getIO()->write('<fg=white>Copie du fichier ' . self::FILE_NAME . ' à la racine.</fg=white>');
+        if (copy($this->getSourcePath($event), $this->getDestPath($event))) {
+            $event->getIO()->write('<fg=green>Copie du fichier ' . self::FILE_NAME . ' effectuée</fg=green>');
         } else {
-            $event->getIO()->write('<fg=red>Erreur lors de la copie du fichier grumphp.yml</fg=red>');
+            $event->getIO()->write('<fg=red>Erreur lors de la copie du fichier ' . self::FILE_NAME . '</fg=red>');
         }
     }
 
-    public static function removeConfigFile(PackageEvent $event)
+    private function removeConfigFile(PackageEvent $event)
     {
-        $event->getIO()->write("<fg=white>Suppression du fichier grumphp.yml à la racine.</fg=white>");
-        if (unlink($event->getComposer()->getConfig()->get('vendor-dir') . '/../grumphp.yml')) {
+        $event->getIO()->write('<fg=white>Suppression du fichier grumphp.yml à la racine.</fg=white>');
+        if (unlink($this->getSourcePath($event))) {
             $event->getIO()->write('<fg=green>Suppression du fichier grumphp.yml effectuée</fg=green>');
         } else {
             $event->getIO()->write('<fg=red>Erreur lors de la suppression du fichier grumphp.yml</fg=red>');
         }
+    }
+
+    private function getSourcePath(PackageEvent $event)
+    {
+        return $event->getComposer()->getConfig()->get('vendor-dir') . '/' . self::PACKAGE_NAME . '/config/' . self::FILE_NAME;
+    }
+
+    private function getDestPath(PackageEvent $event)
+    {
+        return $event->getComposer()->getConfig()->get('vendor-dir') . '/../' . self::FILE_NAME;
     }
 }
