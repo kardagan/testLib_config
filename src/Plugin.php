128551b82cd634c6c287059ef8104f6eeedda40f
Geoffrey Laurent <geoffrey.laurent@rueducommerce.com>
2017-03-05 14:57:14 +0100

    modif

---
+++ src/Plugin.php
@@ -0,0 +1,103 @@
+<?php
+
+
+use Composer\Composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UninstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvents;
+use Composer\Installer\PackageEvent;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    const PACKAGE_NAME = 'kardagan/testlibconf';
+
+    /**
+     * @var Composer
+     */
+    protected $composer;
+
+    /**
+     * @var IOInterface
+     */
+    protected $io;
+
+    /**
+     * @var bool
+     */
+    protected $configureScheduled = false;
+
+    /**
+     * @var bool
+     */
+    protected $initScheduled = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
+    /**
+     * Attach package installation events:
+     *
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            PackageEvents::POST_PACKAGE_INSTALL => 'postPackageInstall',
+            PackageEvents::POST_PACKAGE_UPDATE => 'postPackageUpdate',
+            PackageEvents::PRE_PACKAGE_UNINSTALL => 'prePackageUninstall',
+            ScriptEvents::POST_INSTALL_CMD => 'runScheduledTasks',
+            ScriptEvents::POST_UPDATE_CMD => 'runScheduledTasks',
+        ];
+    }
+
+    /**
+     * When this package is updated, the git hook is also initialized
+     *
+     * @param PackageEvent $event
+     */
+    public function postPackageInstall(PackageEvent $event)
+    {
+        $event->getIO()->write('<fg=red>MON TEST : ' . __FUNCTION__ . '</fg=red>');
+    }
+
+    /**
+     * When this package is updated, the git hook is also updated
+     *
+     * @param PackageEvent $event
+     */
+    public function postPackageUpdate(PackageEvent $event)
+    {
+        $event->getIO()->write('<fg=red>MON TEST : ' . __FUNCTION__ . '</fg=red>');
+    }
+
+    /**
+     * When this package is uninstalled, the generated git hooks need to be removed
+     *
+     * @param PackageEvent $event
+     */
+    public function prePackageUninstall(PackageEvent $event)
+    {
+        $event->getIO()->write('<fg=red>MON TEST : ' . __FUNCTION__ . '</fg=red>');
+    }
+
+    /**
+     * @param Event $event
+     */
+    public function runScheduledTasks(Event $event)
+    {
+        $event->getIO()->write('<fg=red>MON TEST : ' . __FUNCTION__ . '</fg=red>');
+    }
+
+}
